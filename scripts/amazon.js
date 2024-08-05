dee1a66dc4a2dd5d5d8842b564a16ca72246bb9e
AB Singh <abhisheksingh7254>
2024-08-05 14:50:18 +0530

    amazon.js file data bases added

---
+++ scripts/amazon.js
@@ -0,0 +1,26 @@
+const productDetail=[
+  {
+    name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    imgURL:'images/products/athletic-cotton-socks-6-pairs.jpg',
+    rating:{
+      stars:'images/ratings/rating-45.png',
+      count:87
+      },
+    priceCent:1090},
+    {
+      name:'Intermediate Size Basketball',
+      imgURL:'images/products/intermediate-composite-basketball.jpg',
+      rating:{
+        stars:'images/ratings/rating-40.png',
+        count:127
+        },
+      priceCent:2095},
+    {
+      name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
+      imgURL:'images/products/athletic-cotton-socks-6-pairs.jpg',
+      rating:{
+        stars:'images/ratings/rating-45.png',
+        count:87
+        },
+      priceCent:1090 
+    }]
