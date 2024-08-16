f04050599f434ae805df17cf8f8f08f1c658f85a
AB Singh <abhisheksingh7254>
2024-08-16 21:45:29 +0530

    use module to import cart variale and removed script tag

--- data/products.js
+++ data/products.js
@@ -1,5 +1,20 @@
 const products = [
   {
+    id: "id1",
+    image: "images/products/backpack.jpg",
+    name: "My fav Bag 1 fancy grey-1",
+    rating: {
+      stars: 5.0,
+      count: 101
+    },
+    priceCents: 2000,
+    keywords: [
+      
+      "backpack",
+      "black bag"
+    ]
+  },
+  {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,8 +1,8 @@
-
-      //HTML genreted through JAVASCRIPT....
-      let productHTML='';
-      products.forEach((product)=>{
-        productHTML+=`<div class="product-container">
+//HTML genreted through JAVASCRIPT....
+import {cart} from '../data/cart.js';
+let productHTML = "";
+products.forEach((product) => {
+  productHTML += `<div class="product-container">
           <div class="product-image-container">
             <image class="product-image"
               src=${product.image}>
@@ -14,14 +14,14 @@
 
           <div class="product-rating-container">
             <image class="product-rating-stars"
-              src="./images/ratings/rating-${product.rating.stars *10}.png">
+              src="./images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${(product.priceCents /100).toFixed(2)}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -47,69 +47,66 @@
           </div>
 
           <button class="add-to-cart-button button-primary js-add-to-cart" 
-          data-product-id="${product.id}" data-product-rating="${product.rating.stars}">
+          data-product-id="${product.id}" data-product-rating="${
+    product.rating.stars
+  }">
             Add to Cart
           </button>
         </div>`;
-        
-        // console.log(productHTML);
-      });
-      document.querySelector('.js-products-grid').innerHTML=productHTML;
 
-      const addedMessageTimeouts={};                  //to store the setTimeOut returned id for each product
+  // console.log(productHTML);
+});
+document.querySelector(".js-products-grid").innerHTML = productHTML;
+
+const addedMessageTimeouts = {}; //to store the setTimeOut returned id for each product
 //Add to Cart button Functional >>>>
-      document.querySelectorAll('.js-add-to-cart')
-        .forEach((button)=>{
-          button.addEventListener('click',()=>{
-            const {productId}=button.dataset;         //const productId=button.dataset.productId; ---->Destructuring
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    const { productId } = button.dataset; //const productId=button.dataset.productId; ---->Destructuring
 
-            let matchingItem;
-            cart.forEach((item)=>{
-              if(productId===item.productId){
-                matchingItem=item;
-              }
-              });
-              //<select> quantity
-              const quantitySelector=document.querySelector(
-                `.js-quantity-selector-${productId}`
-              );
-              // const quantity = quantitySelector.value; 
-              //it return value as string..so covert it as number using-- Number() method>
-              const quantity=Number(quantitySelector.value);
-              // console.log(quantity);
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+    //<select> quantity
+    const quantitySelector = document.querySelector(
+      `.js-quantity-selector-${productId}`
+    );
+    // const quantity = quantitySelector.value;
+    //it return value as string..so covert it as number using-- Number() method>
+    const quantity = Number(quantitySelector.value);
+    // console.log(quantity);
 
-              if(matchingItem){
-                matchingItem.quantity+=quantity;
-              }else{
-                cart.push({productId,quantity}); //{productId:productId,quantity:quantity} --->Destructuring
-              }
-              
-              let cartQuantity=0;
-              cart.forEach((item)=>{
-                cartQuantity+=item.quantity;
-                // console.log(cart);
-                
-              });
-              const addedMessage = document.querySelector(
-                `.js-added-to-cart-${productId}`
-              );
-              addedMessage.classList.add('js-added-visible');
-              setTimeout(()=>{
-                const prevTimeoutId=addedMessageTimeouts[productId];
-                if(prevTimeoutId){
-                  clearTimeout(prevTimeoutId)
-                }
+    if (matchingItem) {
+      matchingItem.quantity += quantity;
+    } else {
+      cart.push({ productId, quantity }); //{productId:productId,quantity:quantity} --->Destructuring
+    }
 
-                const timeOutId=setTimeout(()=>{
-                  addedMessage.classList.remove('js-added-visible');
-                },2000);
-                addedMessageTimeouts[productId]=timeOutId;
-                console.log(addedMessageTimeouts[productId]);
-              });
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+      // console.log(cart);
+    });
+    const addedMessage = document.querySelector(
+      `.js-added-to-cart-${productId}`
+    );
+    addedMessage.classList.add("js-added-visible");
+    setTimeout(() => {
+      const prevTimeoutId = addedMessageTimeouts[productId];
+      if (prevTimeoutId) {
+        clearTimeout(prevTimeoutId);
+      }
 
-              
-              document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+      const timeOutId = setTimeout(() => {
+        addedMessage.classList.remove("js-added-visible");
+      }, 2000);
+      addedMessageTimeouts[productId] = timeOutId;
+      // console.log(addedMessageTimeouts[productId]);
+    });
 
-             
-          });
-        });
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+  });
+});
